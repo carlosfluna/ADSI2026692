bdc85c4d84b5bdd5fd6b1a45e984e1b768780411
carlosfluna <carlosfluna@misena.edu.co>
2021-03-16 19:40:19 -0500

    partial_views_noche

---
+++ PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Vista2()
+        {
+            return View();
+        }
+        public ActionResult Vista3()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Vistas2()
+        {
+            return View();
+        }
+        public ActionResult Vistas3()
+        {
+            return View();
+        }
+    }
+}
